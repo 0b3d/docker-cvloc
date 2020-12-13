f44377c000fd241e3c2c2ff8a9678d57f3ceb5df
0b3d <samanoabonce@gmail.com>
2020-12-13 01:42:26 +0000

    oxford

--- postgis/initdb-postgis.sh
+++ postgis/initdb-postgis.sh
@@ -6,6 +6,7 @@ CREATE DATABASE london;
 CREATE DATABASE bristol;
 CREATE DATABASE newyork;
 CREATE DATABASE pittsburgh;
+CREATE DATABASE oxford;
 EOSQL
 
 # Create extension in databases -------------------------------------------
@@ -34,12 +35,21 @@ CREATE EXTENSION postgis;
 CREATE EXTENSION hstore;
 EOSQL
 
+# Oxford
+psql --dbname="oxford" --user="postgres" <<- 'EOSQL'
+CREATE EXTENSION postgis;
+CREATE EXTENSION hstore;
+EOSQL
+
+
 # echo "Loading osm to databases"
 cd openstreetmap-carto
 osm2pgsql -s -C 300 -c -G --hstore --style openstreetmap-carto.style --tag-transform-script openstreetmap-carto.lua -d bristol -H localhost -U postgres /Bristol.osm.pbf
 osm2pgsql -s -C 300 -c -G --hstore --style openstreetmap-carto.style --tag-transform-script openstreetmap-carto.lua -d london -H localhost -U postgres /London.osm.pbf
 osm2pgsql -s -C 300 -c -G --hstore --style openstreetmap-carto.style --tag-transform-script openstreetmap-carto.lua -d newyork -H localhost -U postgres /NewYork.osm.pbf
 osm2pgsql -s -C 300 -c -G --hstore --style openstreetmap-carto.style --tag-transform-script openstreetmap-carto.lua -d pittsburgh -H localhost -U postgres /pittsburgh.osm
+osm2pgsql -s -C 300 -c -G --hstore --style openstreetmap-carto.style --tag-transform-script openstreetmap-carto.lua -d oxford -H localhost -U postgres /oxford.osm
+
 
 cd /
 echo "Finished"
